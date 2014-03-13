ff5d5c411dcb269a077d9e95a6192686850fa523
prasidh09 <prasidh09@gmail.com>
2014-03-13 16:22:57 -0400

    initial http get

--- argus/a_capture.py
+++ argus/a_capture.py
@@ -1,3 +1,21 @@
 '''
 Python module to capture BGP hijack data from Argus
 '''
+import urllib.request
+import binascii
+from collections import Counter 
+
+def get():
+    s=urllib.request.urlopen("http://argus.csnet1.cs.tsinghua.edu.cn/api/alarms/100,123/").read()
+    str=s.decode('ascii')
+    str1=str.split(sep=':')
+    print(str)
+    print("\n")
+    for i in str1:
+        print(i)
+        #print(Counter(str1[i]))
+    return s
+
+if __name__ == '__main__':
+    print("Hello") 
+    print(get())

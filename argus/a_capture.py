8487850a0115d6d0f11ef74b85bd354c13fe3217
soujanyas <soujanya@fsl.cs.sunysb.edu>
2014-03-14 02:43:08 -0400

    Read data from Argus using HTTP GET

--- argus/a_capture.py
+++ argus/a_capture.py
@@ -1,67 +1,42 @@
+#!/bin/env/python
+
 '''
 Python module to capture BGP hijack data from Argus
 '''
-import urllib.request
-#import binascii
-import re 
 
-def get():
-    s=urllib.request.urlopen("http://argus.csnet1.cs.tsinghua.edu.cn/api/alarms/100,123/").read()
-    str=s.decode('ascii')
-    dict = []
+import urllib.request
+import re
+
+
+class ArgusCapture:
+
+  def get(self, url):
+    '''
+    Invokes HTTP GET method on the given URL to retrieve BGP updates from Argus
+    '''
+    s = urllib.request.urlopen(url).read()
+    str = s.decode('ascii')
+    dlist = list()
     n = 0
-    #print(str)
+
+    #Get all the entries corresponding to one BGP message
     exp = re.compile(r'\{.+?\}')
     for line in exp.findall(str):
         res = exp.match(line).group().replace("{","").replace("}","").replace("\"", "")
-        list = res.split(",")
-        for pair in list:
-            j = pair.split(":")  
-            print ("count:%d %s" % (len(j), j))          
-            dict.append()
-        
-        print (dict[n])
+        dictobj  = dict()
+        for pair in res.split(","):
+            j = pair.split(":")
+            if(len(j) != 2):
+              assert False, "Format error, key value pair does not exist"
+            dictobj[j[0].strip()] = j[1].strip()
+
+        dlist.append(dictobj)
         n += 1
-    #print("Len: %d" % len(res))
-    
-    #print(res) 
-    return res
+    return dlist
 
 if __name__ == '__main__':
-    #print("Hello") 
-    get()
-
-'''
-Python module to capture BGP hijack data from Argus
-
-import urllib.request
-import binascii
-import re
-from collections import Counter 
-
-def get():
-    s=urllib.request.urlopen("http://argus.csnet1.cs.tsinghua.edu.cn/api/alarms/100,123/").read()
-    str=s.decode('ascii')
-    str.split(sep='[')
-    str1=str.replace("[","").replace("]","").split(sep='},')
-    dict=[]
-    n = 0
-    for i in str1:
-        l=i.split(sep='{')
-        r=[elem.strip().split(',') for elem in l]
-        pairlist=[elem.strip().split(':') for elem in r[1]]
-        for j in pairlist:
-            ind_split = [elem.strip().split('"') for elem in j]
-            #dict[ind_split[0][1]]=ind_split[1][1]
-            print(ind_split[0][1])
-            print(ind_split[1][1])
-            #if(ind_split[0][1]):
-                #dict[n][ind_split[0][1]] = ind_split[1][1]
-        n += 1
-    print (dict)    
-    return str1
-
-if __name__ == '__main__':
-    #print("Hello") 
-    get()
-'''
+    argus_capture = ArgusCapture()
+    res = argus_capture.get("http://argus.csnet1.cs.tsinghua.edu.cn/api/alarms/100,123/")
+    print (res)
+    for index, item in enumerate(res):
+      print ("%d : time:%s" % (index, item["timestamp"]))

9320318a2c0706cd0c6f48dea23a85754095838f
soujanyas <soujanya.shankaranarayana@stonybrook.edu>
2014-03-23 01:43:59 -0400

    Argus prints optimized, some minor bugs fixed

--- argus/a_capture.py
+++ argus/a_capture.py
@@ -45,10 +45,9 @@ class ArgusCapture:
     results = list()
     while i < NUM_ALARMS:
       res = self.get(url + "%d,%d" %(i, i + 99))
-      print ("all list : %d" % len(res))
-      results.append(res)
+      results.extend(res)
       i += 100
-      print ("%d -> %d" % (i, i+100))
+      #print ("%d -> %d" % (i, i+100))
     return results
 
       #for index, item in enumerate(res):
@@ -57,7 +56,7 @@ class ArgusCapture:
   def in_range(self, start_date, end_date, timestamp):
     start = time.mktime(time.strptime(start_date, time_format))
     end = time.mktime(time.strptime(end_date, time_format))
-    print("%s -> %s : %s" %(start, end, timestamp))
+    #print("%s -> %s : %s" %(start, end, timestamp))
     if float(timestamp) > start and float(timestamp) < end:
       return True
     return False
@@ -65,18 +64,20 @@ class ArgusCapture:
   def get_entries_in_range(self, start, end, url):
     all_entries = self.get_all_results(url)
     entries_in_range = []
-
+    print("Alarms/Anomalies in range:\n")
     for index,entry in enumerate(all_entries):
       if(self.in_range(start, end, entry["timestamp"])):
-        print ("Entry in range: %s" % entry)
+        print ("## %s" % entry)
         entries_in_range.append(entry)
     return entries_in_range
 
   def get_alarms_in_range(self, start, end):
+    print("Looking for alarms in range: %s -> %s.." %(start, end))
     alarm_url = self.url + "alarms/"
     return self.get_entries_in_range(start, end, alarm_url)
 
   def get_anomalies_in_range(self, start, end):
+    print("Looking for anomalies in range: %s -> %s..\n" %(start, end))
     anomaly_url = self.url + "anomalous/"
     return self.get_entries_in_range(start, end, anomaly_url)
 
@@ -85,16 +86,16 @@ class ArgusCapture:
 def test_in_range():
   start = "2010-10-01 01:01:01"
   end = "2012-10-01 01:01:01"
-  timestamp = ("%s") % time.mktime(time.strptime("2011-10-01 01:01:01",time_format))
+  timestamp = ("%s") % time.mktime(time.strptime("2011-04-01 01:01:01",time_format))
   assert ArgusCapture().in_range(start, end, timestamp)
-  timestamp = ("%s") % time.mktime(time.strptime("2014-10-01 01:01:01",time_format))
+  timestamp = ("%s") % time.mktime(time.strptime("2014-04-30 01:01:01",time_format))
   assert (not ArgusCapture().in_range(start, end, timestamp))
 
 
 
 if __name__ == '__main__':
     argus_capture = ArgusCapture()
-    argus_capture.get_alarms_in_range("2013-12-01 01:01:01", "2014-01-01 01:01:01")
+    argus_capture.get_alarms_in_range("2012-04-01 01:01:01", "2012-04-30 01:01:01")
     argus_capture.get_anomalies_in_range("2013-12-01 01:01:01", "2014-01-01 01:01:01")
     test_in_range()
     #for index, item in enumerate(res):
